5bd5b254233c93a668e02bd41da4bab8b80606da
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-29 18:48:02 -0400

    Add interceptors to backendclients

--- src/Clients/backendClient.js
+++ src/Clients/backendClient.js
@@ -1,5 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const backendClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+const backendClient = axios.create({
+  baseURL: "http://localhost:3000/api",
 });
+
+// Add token before each request
+backendClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("Stoken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default backendClient;
