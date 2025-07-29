77cae489925e5f70b9a71f745c606d09163238fc
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-29 18:49:28 -0400

    Add all functionaltiy to parent component App.jsx (TEST)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import TransactionPage from "./Pages/TransactionPage"
+import LoginPage from "./Pages/LoginPage"
+import SignupPage from "./Pages/SignupPage"
+import Homepage from "./Pages/HomePage"
+import Dashboard from "./Pages/Dashboard"
+import ReportsPage from "./Pages/ReportsPage"
+import { Routes,Route } from "react-router-dom"
+import PrivateRoute from "./components/PrivateRoute"
+import Navbar from "./components/Navbar"
+import Footbar from "./components/Footbar"
 
-import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+         <Route path="/" element={<Homepage/>} />
+         <Route path="/Signup" element={<SignupPage />} />
+         <Route path="/login" element={<LoginPage />} />
+
+         {/*  Only logged-in users can see this */}
+          <Route path="/reports"
+          element={<PrivateRoute>
+                <ReportsPage />
+              </PrivateRoute>} />
+        <Route path="/dashboard"
+         element={<PrivateRoute>
+              <Dashboard />           
+              </PrivateRoute>} />
+        <Route path="/transaction" 
+        element={<PrivateRoute>
+              <TransactionPage />         
+            </PrivateRoute>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+      <Footbar />
     </>
   )
 }
