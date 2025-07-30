01477769a5686841132608d0579f3cf9eec53683
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-30 11:52:24 -0400

    Update context to handle logout to homepage

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-
+import {useNavigate} from 'react-router-dom';
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 
@@ -7,6 +7,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
   const [loading, setLoading] = useState(true); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedToken = localStorage.getItem("Stoken");
@@ -34,6 +35,7 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("Stoken");
     setUser(null);
     setToken(null);
+    navigate("/");
   };
 
   return (
