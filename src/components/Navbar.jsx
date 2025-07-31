3b5d486afe25859c651ef2580cb6d9aedadf04e1
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-31 17:03:22 -0400

    Add activeAccount name at Navbar to show name fo the account

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,29 +2,55 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../Context/useAuth";
 
 function Navbar() {
-    const { user,logout } = useAuth();
+  const { user, logout, activeAccountName } = useAuth();
   return (
     <nav className="bg-white shadow px-4 py-3 flex justify-between items-center">
       <NavLink to="/" className="text-xl font-bold text-blue-700">
         SmartSpend
       </NavLink>
-
+      <div className="flex gap-6 items-center">
+        {activeAccountName && (
+          <span className="ms-3 text-sm text-blue-500">
+             {activeAccountName}
+          </span>
+        )}
+      </div>
       <div className="space-x-4">
         {!user ? (
           <>
-            <NavLink to="/" className="text-blue-600">Home</NavLink>
-            <NavLink to="/login" className="text-blue-600">Login</NavLink>
-            <NavLink to="/signup" className="text-blue-600">Signup</NavLink>
+            <NavLink to="/" className="text-blue-600">
+              Home
+            </NavLink>
+            <NavLink to="/login" className="text-blue-600">
+              Login
+            </NavLink>
+            <NavLink to="/signup" className="text-blue-600">
+              Signup
+            </NavLink>
           </>
         ) : (
           <>
-            <NavLink to="/dashboard" className="text-blue-600">Dashboard</NavLink>
-            <NavLink to="/transaction" className="text-blue-600">Transactions</NavLink>
-            <NavLink to="/reports" className="text-blue-600">Reports</NavLink>
-            <NavLink to="/category" className="text-blue-600">Category</NavLink>
-            <NavLink to="/profile" className="text-blue-600">Profile</NavLink>
-            <NavLink to="/settings" className="text-blue-600">Settings</NavLink>
-            <button onClick={logout} className="text-red-500">Logout</button>
+            <NavLink to="/dashboard" className="text-blue-600">
+              Dashboard
+            </NavLink>
+            <NavLink to="/transaction" className="text-blue-600">
+              Transactions
+            </NavLink>
+            <NavLink to="/reports" className="text-blue-600">
+              Reports
+            </NavLink>
+            <NavLink to="/category" className="text-blue-600">
+              Category
+            </NavLink>
+            <NavLink to="/profile" className="text-blue-600">
+              Profile
+            </NavLink>
+            <NavLink to="/settings" className="text-blue-600">
+              Settings
+            </NavLink>
+            <button onClick={logout} className="text-red-500">
+              Logout
+            </button>
           </>
         )}
       </div>
@@ -33,4 +59,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
